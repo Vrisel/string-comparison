d3a57f29f661b0894c4a4809e3f22babad7e9a50
브리셀_집 <vrisel.p@gmail.com>
2026-04-26 00:24:08 +0900

    FEAT: 이것저것 개선 (...)

--- bana.js
+++ bana.js
@@ -1,28 +1,83 @@
-function banaLint(str) {
-	let dict = {
-		"`":"@", "~":"^", "{":"[", "}":"]", "|":"\\",
-		"“":'"', "”":'"',
-		"’":"'", "‘":"'"
-	};
-	return str.replace(
-		/./gi,
-		c => c in dict ? dict[c] : c.toUpperCase()
-	);
+function banaLint(str, replacer) {
+  let dict = {
+    '`': '@',
+    '~': '^',
+    '|': '\\',
+    '{': '[',
+    '}': ']',
+    '“': '"',
+    '”': '"',
+    '‘': "'",
+    '’': "'",
+  };
+  return str.replace(/\r\n?|./gis, (c) => {
+    if ('\r\n'.includes(c)) {
+      return '\n';
+    } else if (c in dict) {
+      return dict[c];
+    } else {
+      return c.toLowerCase();
+    }
+  });
 }
 
-function banaToUnicode(str) {
-	const bana = ' A1B\'K2L@CIF/MSP"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\\0Z7(_?W]#Y)=';
-	return str.replace(
-		/./gi,
-		c => {
-			//const charCode = c.charCodeAt();
-			//if (charCode>=0x2800 && charCode<=0x28ff) return c;
-			if (c>='⠀' && c<='⠿') return c;
-			
-			let idx = bana.indexOf(c);
-			if (idx >= 0) return String.fromCharCode(0x2800 + idx)
-			if ('\r\n↵⏎'.includes(c)) return '⏎'
-			else return String.fromCharCode(0x2800) //throw new Error("유니코드 변환 실패")
-		}
-	);
+const banaOrder =
+  ' a1b\'k2l@cif/msp"e3h9O6r^djg>ntq,*5<-u8v.%[$+x!&;:4\\0z7(_?w]#y)=';
+
+function banaToUnicode(str, replacer) {
+  return banaLint(str).replace(/\r\n?|./gis, (c) => {
+    // (0x2800 ~ 0x28ff)
+    if (c >= '⠀' && c <= '⠿') return c;
+    else if ('\r\n'.includes(c)) return '\n';
+
+    const idx = banaOrder.indexOf(c);
+    if (idx >= 0) return String.fromCharCode(0x2800 + idx);
+    else {
+      switch (typeof replacer) {
+        case 'string':
+          return replacer;
+        case 'number':
+          //case 'bigint':
+          return String.fromCharCode(replacer);
+        case 'function':
+          return replacer(c);
+        case 'boolean':
+          // (0x2800, 0xFFFD)
+          return replacer ? '⠀' : '�';
+        case 'undefined':
+        default:
+          //throw new Error("유니코드 변환 실패")
+          return c;
+      }
+    }
+  });
+}
+
+function unicodeToBana(str, replacer) {
+  return str.replace(/\r\n?|./gis, (c) => {
+    const code = c.charCodeAt();
+    if (code >= 0x2800 && code <= 0x28ff) return banaOrder[code - 0x2800];
+    else if ('\r\n'.includes(c)) return '\n';
+
+    const cLint = banaLint(c);
+    if (banaOrder.includes(cLint)) return cLint;
+    else {
+      switch (typeof replacer) {
+        case 'string':
+          return replacer;
+        case 'number':
+          //case 'bigint':
+          return String.fromCharCode(replacer);
+        case 'function':
+          return replacer(c);
+        case 'boolean':
+          // (space, 0xFFFD)
+          return replacer ? ' ' : '�';
+        case 'undefined':
+        default:
+          //throw new Error("유니코드 변환 실패")
+          return c;
+      }
+    }
+  });
 }
--- script.js
+++ script.js
@@ -1,61 +1,97 @@
 function 비교(e) {
-	e.preventDefault();
-	try{
-	
-        const corr = banaToUnicode(banaLint(correctArea.value));
-        correctArea.value = corr;
-        const inpu = banaToUnicode(banaLint(inputArea.value));
-        inputArea.value = inpu;
-        
-        const compResult = needlemanWunschAlignment(corr, inpu);
+  e.preventDefault();
+  try {
+    const form = document.forms[0];
+    const selectedOption = form.elements['options'].value;
 
-        resultArea.replaceChildren();
-        
-        let currentRow = document.createElement('div');
-        currentRow.className = 'row';
+    let strCorrect = form.elements['correctArea'].value;
+    let strInput = form.elements['inputArea'].value;
 
-        for (const {
-            charCorrect,
-            charInput,
-            stat
-        } of compResult) {
-            let child;
-            if (stat == 'match') {
-                child = document.createElement('span');
-                child.append(charCorrect);
-            }
-            else {
-                child = document.createElement('table');
-                child.border = 0;
-                child.className = stat;
-                
-                for (const c of [charCorrect, charInput]) {
-                    const tr = document.createElement('tr');
-                    const td = document.createElement('td');
-                    
-                    td.append(
-                        c == null
-                        ? 'x' //'❌'
-                        : '\r\n↵⏎'.includes(c)
-                        ? '⏎'
-                        : c
-                    );
-                    tr.appendChild(td);
-                    child.appendChild(tr);
-                }
-            }
-            
-            currentRow.appendChild(child);
-            
-            if ('\r\n↵⏎'.includes(charCorrect)) {
-                resultArea.appendChild(currentRow);
-                currentRow = document.createElement('div');
-                currentRow.className = 'row';
-            }
-        }
-        
+    switch (selectedOption) {
+      case 'toBana':
+        strCorrect = unicodeToBana(strCorrect, false);
+        strInput = unicodeToBana(strInput, false);
+        break;
+      case 'toUnicode':
+        strCorrect = banaToUnicode(strCorrect, true);
+        strInput = banaToUnicode(strInput, true);
+        break;
+      default:
+        break;
+    }
+    /*if (isBana.checked) {
+      strCorrect = banaLint(strCorrect);
+      strInput = banaLint(strInput);
+    }
+    if (toUnicode.checked) {
+      strCorrect = banaToUnicode(strCorrect);
+      strInput = banaToUnicode(strInput);
+    }
+
+    //correctArea.value = strCorrect;
+    //inputArea.value = strInput;*/
+
+    const compResult = needlemanWunschAlignment(strCorrect, strInput);
+
+    resultArea.replaceChildren();
+    resultArea.classList.toggle('braille', selectedOption === 'toBana');
+
+    let currentRow = document.createElement('div');
+    currentRow.className = 'row';
+
+    for (const { charCorrect, charInput, stat } of compResult) {
+      let child;
+      if (stat == 'match') {
+        child = document.createElement('span');
+        child.append('\r\n↵⏎'.includes(charCorrect) ? '⏎' : charCorrect);
+      } else {
+        child = document.createElement('table');
+        child.border = 0;
+        child.className = stat;
+        const trCorrect = document.createElement('tr');
+        const tdCorrect = document.createElement('td');
+        let htmlCorrect =
+          stat === 'insertion'
+            ? '↑'
+            : charCorrect == null
+              ? '␡'
+              : '\r\n↵⏎'.includes(charCorrect)
+                ? '⏎'
+                : charCorrect;
+        if (stat === 'deletion') htmlCorrect = `<ins>${htmlCorrect}</ins>`;
+        tdCorrect.insertAdjacentHTML('beforeend', htmlCorrect);
+        trCorrect.appendChild(tdCorrect);
+        child.appendChild(trCorrect);
+
+        const trInput = document.createElement('tr');
+        const tdInput = document.createElement('td');
+        let htmlInput =
+          stat === 'deletion'
+            ? '↓'
+            : charInput == null
+              ? '␡'
+              : '\r\n↵⏎'.includes(charInput)
+                ? '⏎'
+                : charInput;
+        if (stat !== 'deletion') htmlInput = `<del>${htmlInput}</del>`;
+        tdInput.insertAdjacentHTML('beforeend', htmlInput);
+        trInput.appendChild(tdInput);
+        child.appendChild(trInput);
+      }
+
+      currentRow.appendChild(child);
+
+      if ('\r\n↵⏎'.includes(charCorrect)) {
         resultArea.appendChild(currentRow);
-        
-        return false;
-	} catch (error) { alert(error); }
+        currentRow = document.createElement('div');
+        currentRow.className = 'row';
+      }
+    }
+
+    resultArea.appendChild(currentRow);
+
+    return false;
+  } catch (error) {
+    alert(error);
+  }
 }
